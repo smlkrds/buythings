50d9f03f93cda509d5cc9ca472ae25e09c1a4239
smlkrdas <smlkrdas@gmail.com>
2023-01-28 01:22:38 +0300

    product model is improved

--- buythings/base/models.py
+++ buythings/base/models.py
@@ -5,5 +5,15 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     price = models.IntegerField(default=0)
+    active = models.BooleanField(default=True)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
     def __str__(self):
         return self.name
+
+class Comment(models.Model):
+    content = models.CharField(max_length=200)
+    commented_product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    commentator = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.content
