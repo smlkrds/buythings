9758012541f9a7df7cc1ce4ec05f1a9e0d4c4cab
smlkrdas <smlkrdas@gmail.com>
2023-01-29 19:16:38 +0300

    login page has been added

--- buythings/accounts/urls.py
+++ buythings/accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('login/', views.login_request, name='login'),
 ]
--- buythings/accounts/views.py
+++ buythings/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 
 def signup(request):
     if request.method == 'POST':
@@ -15,3 +17,21 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f'You are now logged in as {username}')
+                return redirect('index')
+            else:
+                messages.error(request, 'Invalid username ır password')
+        else:
+            messages.error(request, 'Invalid username or password')
+    form = AuthenticationForm()
+    return render(request=request, template_name='accounts/registration.html', context={'login_form':form})
