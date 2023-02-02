342a3de7d0a7145005952bfdcdb0a45016346137
smlkrdas <smlkrdas@gmail.com>
2023-02-02 14:14:59 +0300

    02/02/2023 14:14

--- buythings/base/models.py
+++ buythings/base/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    category = models.CharField(max_length=50, default="null")
+    def __str__(self):
+        return self.category
 class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     price = models.IntegerField(default=0)
     active = models.BooleanField(default=True)
     seller = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
--- buythings/base/views.py
+++ buythings/base/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import HttpResponse, render
 
-from .models import Product
+from .models import Product, Category
+
 def index(request):
 	products = Product.objects.all()
 	return render(request, 'base/index.html', {'products': products})
+
+def categories(request):
+	category_list = Category.objects.all()
+	return render(request, 'base/categories.html', {'categories': category_list})
