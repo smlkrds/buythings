d736b3968a4c032a32e188979764f49ec5ce3713
Şamil Kardaş <87974874+smlkrds@users.noreply.github.com>
2023-03-13 14:11:35 +0300

    Update settings.py

--- buythings/buythings/settings.py
+++ buythings/buythings/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3f9b84)+*qqsg79(*8y@)+!#59^m-=o!$h9(884@s*+=6l@fa&'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
